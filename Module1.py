dedf5f77079ef9cc7f8363f74c82f52397c14222
Gautamsiue <144144735+Gautamsiue@users.noreply.github.com>
2023-10-16 21:31:24 -0500

    Create Module1.py

---
+++ Module1.py
@@ -0,0 +1,15 @@
+
+
+def process_raw_data(raw_data):
+    """
+    This module takes raw data as input and processes it.
+    
+    Parameters:
+    - raw_data (str): Raw data to be processed.
+
+    Returns:
+    - processed_data (str): Processed data.
+    """
+    # Your processing logic here
+    processed_data = raw_data.upper()
+    return processed_data
